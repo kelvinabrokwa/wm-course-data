0aa9bd8c42cc122420b6ce801495cbfe8c42512b
kelvinabrokwa <kelvinabrokwa@gmail.com>
2016-10-06 17:39:38 -0400

    use redis

--- server.py
+++ server.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3.5
 import sys
 sys.path.append('.')
+import os
 import json
+import redis
 from flask import Flask
 from flask_cors import CORS, cross_origin
 from course_data import get_course_data
 
+
+# Flask app
 app = Flask(__name__)
 CORS(app)
 
-courses = None
+# Redis
+r = redis.from_url(os.environ.get("REDIS_URL"))
 
+if r.get('courses') is None:
+    r.set('courses', json.dumps(get_course_data()))
 
 @app.route('/courses')
-def hello_world():
-    global courses
-    if courses is None:
-        courses = get_course_data()
-    return json.dumps(courses)
+def courses():
+    # Todo: check for data staleness
+    return r.get('courses')
