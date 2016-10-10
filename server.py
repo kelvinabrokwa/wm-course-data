1cda2312b81df8af06404b12613c70ed3f8c530f
kelvinabrokwa <kelvinabrokwa@gmail.com>
2016-10-10 03:03:27 -0400

    finish conflict resolution

--- server.py
+++ server.py
@@ -5,22 +5,15 @@ import os
 import json
 import redis
 from flask import Flask
-<<<<<<< HEAD
 from flask_cors import CORS
 from flask_compress import Compress
-=======
-from flask_cors import CORS, cross_origin
->>>>>>> 0aa9bd8c42cc122420b6ce801495cbfe8c42512b
 from course_data import get_course_data
 
 
 # Flask app
 app = Flask(__name__)
 CORS(app)
-<<<<<<< HEAD
 Compress(app)
-=======
->>>>>>> 0aa9bd8c42cc122420b6ce801495cbfe8c42512b
 
 # Redis
 r = redis.from_url(os.environ.get("REDIS_URL"))
